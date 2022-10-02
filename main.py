c7a5628c6ff10ebc75e53d30d998e4c1f09f8602
ddilyam <dilmd@icloud.com>
2022-10-02 18:00:18 +0600

    1

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def Hello():
-    def init (self, name):
-        self.name = name
-        print(f'Hello, {self.name}')
+#1
+# class Bank:
+#     def __init__(self):
+#         self.balanc = 0
+#
+#     def withdraw(self, amount):
+#         self.balanc -= amount
+#         return self.balanc
+#
+#     def deposit(self, amount):
+#         self.balanc += amount
+#         return self.balanc
+#
+#
+# m1 = Bank()
+# m2 = Bank()
+# m1.deposit(100)
+# m1.withdraw(100)
 
-greet_me = Hello('Dilya')
+
+#2
+class Money:
+    number: [int]  # number in money (like 100$, so 100 is a number)
+    currency: [string]  # currency, on of USD, EUR, RUB, TNG
+    amount: [int]  # amount of that money (like I have 5 of 100$, so 5 is amount)
+
+    def to_tenge():
+
+
+    def __str__():
+        pass
+
+
+class Wallet:
+    money: [list]
+
+    def __str__():
+        pass
